ee0f47023069f1edb87044af8ed24b5f580be793
Meowtomata <its.anatoliy@gmail.com>
2025-11-08 11:17:19 -0600

    Override :Obsidian paste_img to support .jpg

--- modules/nvf/ide.nix
+++ modules/nvf/ide.nix
@@ -77,23 +77,57 @@
         desc = "Buffers Find";
       }
     ];
+    # Allows me to import lua code in files under the ./nvim/lua directory
+    additionalRuntimePaths = [ ./nvim ];
 
     /*
-      I find it more readable (and potentially concise) to set my keymaps in lua
-      So I will set them in native lua here
+        :Obsidian paste_img from obsidian-nvim/obsidian.nvim
+        (and :ObsidianPasteImg from epwalsh/obsidian.nvim)
 
-      If I wanted to split these out into modules, I could:
-      1. add additionalRuntimePaths = [ ./nvim ],
-      2. put my keymaps in a module like ./nvim/lua/keymaps.lua,
-      3. add require('keymaps') below
+        Only supports .png format for pasting
+        If I do, wl-copy < image.jpg, it will tell me that
+        there is no image detected in clipboard
 
-      mappings is also the module name used by nvf to set keymaps
-      naming the below mappings will not clear other keymaps
-      feel free to check output with `nvf-print-config | nvim -c 'set filetype=lua`
+        So I used the code from the obsidian-nvim/obsidian.nvim repo
+        and modified it to support image/jpeg (and eventually other formats too)
 
-      I believe if I ever wanted to override existing lua functions
-      I could do so below as well
+        Allowed by additionalRunTimePaths = [ ./nvim ],
+        this code is placed under:
+        ./nvim/lua/img_paste_override.lua
+        ./nvim/lua/paste_img_override.lua
+        and should run in that order
     */
+    luaConfigRC.obsidian-paste-img-override = # lua
+      ''
+        -- import ./nvim/lua/img_paste_override.lua module
+        require('img_paste_override')
+        local status_ok, custom_img_paste = pcall(require, "img_paste_override")
+
+        if status_ok then
+          -- overwrite :Obsidian paste_img helper function to allow pasting .jpg imgs
+          package.loaded["obsidian.img_paste"] = custom_img_paste
+        else
+          vim.notify(
+            "Could not load custom obsidian image paste module. Using the default.",
+            vim.log.levels.WARN
+          )
+        end
+
+        -- import ./nvim/lua/paste_img_override.lua module
+        require('paste_img_override')
+        local status_ok, custom_img_paste = pcall(require, "paste_img_override")
+
+        if status_ok then
+          -- overwrite :Obsidian paste_img with function that accepts .jpg imgs
+          package.loaded["obsidian.commands.paste_img"] = custom_img_paste
+        else
+          vim.notify(
+            "Could not load custom obsidian image paste module. Using the default.",
+            vim.log.levels.WARN
+          )
+        end
+      '';
+
     luaConfigRC.mappings = # lua
       ''
         -- function to swap opts and rhs for improved readability
---
+++ modules/nvf/nvim/lua/img_paste_override.lua
@@ -0,0 +1,186 @@
+local Path = require "obsidian.path"
+local log = require "obsidian.log"
+local run_job = require("obsidian.async").run_job
+local api = require "obsidian.api"
+local util = require "obsidian.util"
+
+local M = {}
+
+-- Image pasting adapted from https://github.com/ekickx/clipboard-image.nvim
+
+---@return string
+local function get_clip_check_command()
+  local check_cmd
+  local this_os = api.get_os()
+  if this_os == api.OSType.Linux or this_os == api.OSType.FreeBSD then
+    local display_server = os.getenv "XDG_SESSION_TYPE"
+    if display_server == "x11" or display_server == "tty" then
+      check_cmd = "xclip -selection clipboard -o -t TARGETS"
+    elseif display_server == "wayland" then
+      check_cmd = "wl-paste --list-types"
+    end
+  elseif this_os == api.OSType.Darwin then
+    check_cmd = "pngpaste -b 2>&1"
+  elseif this_os == api.OSType.Windows or this_os == api.OSType.Wsl then
+    check_cmd = 'powershell.exe "Get-Clipboard -Format Image"'
+  else
+    error("image saving not implemented for OS '" .. this_os .. "'")
+  end
+  return check_cmd
+end
+
+--- Check if clipboard contains image data.
+---
+---@return boolean
+function M.clipboard_is_img()
+  local check_cmd = get_clip_check_command()
+  local result_string = vim.fn.system(check_cmd)
+  local content = vim.split(result_string, "\n")
+
+  local is_img = false
+  -- See: [Data URI scheme](https://en.wikipedia.org/wiki/Data_URI_scheme)
+  local this_os = api.get_os()
+  if this_os == api.OSType.Linux or this_os == api.OSType.FreeBSD then
+    if vim.tbl_contains(content, "image/png") then
+      is_img = true
+    elseif vim.tbl_contains(content, "text/uri-list") then
+      local success =
+        os.execute "wl-paste --type text/uri-list | sed 's|file://||' | head -n1 | tr -d '[:space:]' | xargs -I{} sh -c 'wl-copy < \"$1\"' _ {}"
+      is_img = success == 0
+    end
+  elseif this_os == api.OSType.Darwin then
+    is_img = string.sub(content[1], 1, 9) == "iVBORw0KG" -- Magic png number in base64
+  elseif this_os == api.OSType.Windows or this_os == api.OSType.Wsl then
+    is_img = content ~= nil
+  else
+    error("image saving not implemented for OS '" .. this_os .. "'")
+  end
+  return is_img
+end
+
+--- Get the type of image on the clipboard.
+---
+---@return "png"|"jpeg"|nil
+function M.get_clipboard_img_type()
+  local this_os = api.get_os()
+  if this_os ~= api.OSType.Linux and this_os ~= api.OSType.FreeBSD then
+    -- For other OSes, maintain original behavior
+    local check_cmd = get_clip_check_command()
+    local result_string = vim.fn.system(check_cmd)
+    local content = vim.split(result_string, "\n")
+    if this_os == api.OSType.Darwin then
+      if string.sub(content[1], 1, 9) == "iVBORw0KG" then
+        return "png"
+      end
+    elseif this_os == api.OSType.Windows or this_os == api.OSType.Wsl then
+      if content ~= nil and #content > 0 and content[1] ~= "" then
+        return "png"
+      end
+    end
+    return nil
+  end
+
+  local check_cmd = get_clip_check_command()
+  local result_string = vim.fn.system(check_cmd)
+  local content = vim.split(result_string, "\n")
+
+  if vim.tbl_contains(content, "image/png") then
+    return "png"
+  elseif vim.tbl_contains(content, "image/jpeg") then
+    return "jpeg"
+  elseif vim.tbl_contains(content, "text/uri-list") then
+    local success =
+      os.execute "wl-paste --type text/uri-list | sed 's|file://||' | head -n1 | tr -d '[:space:]' | xargs -I{} sh -c 'wl-copy < \"$1\"' _ {}"
+    if success == 0 then
+      -- Re-check for image type after potential conversion
+      result_string = vim.fn.system(check_cmd)
+      content = vim.split(result_string, "\n")
+      if vim.tbl_contains(content, "image/png") then
+        return "png"
+      elseif vim.tbl_contains(content, "image/jpeg") then
+        return "jpeg"
+      end
+    end
+  end
+  return nil
+end
+
+--- TODO: refactor with run_job?
+
+--- Save image from clipboard to `path`.
+---@param path string
+---@param img_type "png" | "jpeg"
+---
+---@return boolean|integer|? result
+local function save_clipboard_image(path, img_type)
+  local this_os = api.get_os()
+
+  if this_os == api.OSType.Linux or this_os == api.OSType.FreeBSD then
+    local mime_type = "image/" .. img_type
+    local cmd
+    local display_server = os.getenv "XDG_SESSION_TYPE"
+    if display_server == "x11" or display_server == "tty" then
+      cmd = string.format("xclip -selection clipboard -t %s -o > '%s'", mime_type, path)
+    elseif display_server == "wayland" then
+      cmd = string.format("wl-paste --no-newline --type %s > %s", mime_type, vim.fn.shellescape(path))
+      return run_job { "bash", "-c", cmd }
+    end
+
+    local result = os.execute(cmd)
+    if type(result) == "number" and result > 0 then
+      return false
+    else
+      return result
+    end
+  elseif this_os == api.OSType.Windows or this_os == api.OSType.Wsl then
+    local cmd = 'powershell.exe -c "'
+      .. string.format("(get-clipboard -format image).save('%s', 'png')", string.gsub(path, "/", "\\"))
+      .. '"'
+    return os.execute(cmd)
+  elseif this_os == api.OSType.Darwin then
+    return run_job { "pngpaste", path }
+  else
+    error("image saving not implemented for OS '" .. this_os .. "'")
+  end
+end
+
+--- @param path string image_path The absolute path to the image file.
+M.paste = function(path, img_type)
+  if util.contains_invalid_characters(path) then
+    log.warn "Links will not work with file names containing any of these characters in Obsidian: # ^ [ ] |"
+  end
+
+  ---@diagnostic disable-next-line: cast-local-type
+  path = Path.new(path)
+
+  -- Make sure fname ends with ".png"
+  if not path.suffix then
+    ---@diagnostic disable-next-line: cast-local-type
+    path = path:with_suffix("." .. img_type)
+  elseif not (path.suffix == "." .. img_type or (img_type == "jpeg" and path.suffix == ".jpg")) then
+    local expected_suffix = (img_type == "jpeg") and ".jpeg' or '.jpg" or "." .. img_type
+    return log.err("invalid suffix for image name '%s', must be '%s'", path.suffix, expected_suffix)
+  end
+
+  if Obsidian.opts.attachments.confirm_img_paste then
+    -- Get confirmation from user.
+    if not api.confirm("Saving image to '" .. tostring(path) .. "'. Do you want to continue?") then
+      return log.warn "Paste aborted"
+    end
+  end
+
+  -- Ensure parent directory exists.
+  assert(path:parent()):mkdir { exist_ok = true, parents = true }
+
+  -- Paste image.
+  local result = save_clipboard_image(tostring(path), img_type)
+  if result == false then
+    log.err "Failed to save image"
+    return
+  end
+
+  local img_text = Obsidian.opts.attachments.img_text_func(path)
+  vim.api.nvim_put({ img_text }, "c", true, false)
+end
+
+return M
